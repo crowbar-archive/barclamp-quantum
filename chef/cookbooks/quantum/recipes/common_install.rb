22c253876de9915df925ebbe9f6a56d1cd8024c6
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-06-06 16:53:48 +0400

    fix for support venv

--- chef/cookbooks/quantum/recipes/common_install.rb
+++ chef/cookbooks/quantum/recipes/common_install.rb
@@ -223,7 +223,14 @@ end
 vlan_start = node[:network][:networks][:nova_fixed][:vlan]
 vlan_end = vlan_start + 2000
 
-link "/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini" do
+
+ovs_quantum_plugin = "/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini"
+
+if quantum[:quantum][:use_gitrepo] == true
+  ovs_quantum_plugin = File.join("/opt/quantum",ovs_quantum_plugin)
+end
+
+link ovs_quantum_plugin do
   to "/etc/quantum/quantum.conf"
   notifies :restart, resources(:service => quantum_agent), :immediately
   notifies :restart, resources(:service => "openvswitch-switch"), :immediately
